b8b2d69f89dbe0701925a0999c6582f5bde5e9d3
PhoebeSothern <78451193+PhoebeSothern@users.noreply.github.com>
2022-04-25 15:31:49 -0700

    Tutorial 3.2.8 Complete

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,8 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRB;
     public float jumpForce;
     public float gravityModifier;
-    public bool isOnGround = true; 
+    public bool isOnGround = true;
+    public bool gameOver = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,6 +28,14 @@ public class PlayerController : MonoBehaviour
     }
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+        else if(collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            Debug.Log("Game Over!");
+        }
     }
 }
